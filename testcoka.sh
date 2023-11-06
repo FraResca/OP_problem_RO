3edc957e2e54b87926ad93b59f4acf0cd417e861
FraResca <francesco.resca@edu.unife.it>
2023-11-07 00:11:10 +0100

    commit per coka in parallelo

--- testcoka.sh
+++ testcoka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sbacth chao_64_1.slurm
-sbacth chao_64_2.slurm
+sbatch chao_64_1.slurm
+sbatch chao_64_2.slurm
 sbatch tsili_1_1.slurm
 sbatch tsili_1_2.slurm
 sbatch tsili_2_1.slurm
-sbacth chao_66_1.slurm
-sbacth chao_66_2.slurm
+sbatch chao_66_1.slurm
+sbatch chao_66_2.slurm
 sbatch tsili_2_2.slurm
 sbatch tsili_3_1.slurm
 sbatch tsili_3_2.slurm
